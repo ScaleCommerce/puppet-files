5b6c58684d09d0c5a4089a47c0b61193520dce02
Thomas Lohner <tl@scale.sc>
2016-05-06 10:02:18 +0200

    add basic functionality

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,29 @@
 # == Class: sc_files
 #
-# Full description of class sc_files here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# Creates file-resources from hiera hash
 #
 # === Examples
 #
-#  class { 'sc_files':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+#  sc_files::files:
+#    '/tmp/test.txt':
+#      ensure: present
+#      content: 'Foo'
+#    '/tmp/test/':
+#      ensure: directory
+#      owner: root
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Thomas Lohner <tl@scale.sc>
 #
 # === Copyright
 #
-# Copyright 2016 Your name here, unless otherwise noted.
+# Copyright 2016 ScaleCommerce GmbH.
 #
 class sc_files {
 
+  $template_vars = hiera_hash('sc_files::template_vars', {})
+  $files = hiera_hash('sc_files::files', {})
+  create_resources('file', $files)
 
 }
